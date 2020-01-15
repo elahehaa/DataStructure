94d5d3eb248ce137500e95b5202f0e0163f1ff1e
Elaheh Aghaarabi <elaheh.a.arabi@gmail.com>
2020-01-15 15:47:42 -0500

    Add files via upload
    TwoDimensionalArray

--- main.cpp
+++ main.cpp
@@ -8,134 +8,133 @@
  * File:   main.cpp
  * Author: Elaheh Aghaarabi
  *
- * Created on November 6, 2019, 8:22 PM
+ * Created on October 24, 2019, 11:58 AM
  */
 
 #include <iostream>
 #include <string>
 
-using namespace std;
 
-class Movie
-{
-public:
-    
-    
-        string movieName;
-        string mpaaRate;
-        int terrible ;
-        int bad ;
-        int okm ;
-        int good ;
-        int great ;
-        
-       void addRating (int rate );
-       double getAverage ();
-       
-       Movie (string name , string mpaa);
-       Movie ();
-         
-};
+using namespace std;
 
 /*
  * 
  */
 int main() {
     
-    Movie m1("Interstellar" , "PG-13") , m2 , m3;
+    int i, j , row , column , number;
+    char letter;
+    bool exit=0;
+    
+    char seat[7][4] = 
+    {
+        {'A','B','C','D'},
+        {'A','B','C','D'},
+        {'A','B','C','D'},
+        {'A','B','C','D'},
+        {'A','B','C','D'},
+        {'A','B','C','D'},
+        {'A','B','C','D'}
+        
+    };
+    
+    char fullSeat [7][4]= {
+        {'X','X','X','X'},
+        {'X','X','X','X'},
+        {'X','X','X','X'},
+        {'X','X','X','X'},
+        {'X','X','X','X'},
+        {'X','X','X','X'},
+        {'X','X','X','X'}
+    };
     
     
-    m2.movieName = "Interstellar";
-    m2.mpaaRate = "PG-13";
-    m2.addRating (1);
-    m2.addRating (2);
-    m2.addRating (2);
-    m2.addRating (3);
-    m2.addRating (3);
-    m2.addRating (3);
-    m2.addRating (4);
-    m2.addRating (4);
-    m2.addRating (4);
-    m2.addRating (5);
-    m2.addRating (5);
-    m2.addRating (5);
-    m2.addRating (5);
-    m2.addRating (5);
+    for ( i=0;i<7;i++)
+    {
+         cout << i+1 << " ";
+        for ( j=0;j < 4; j++)
+        {
+    cout << seat[i][j] << " ";
+        }
+       
+        
+        cout << endl;
+    }
+    do
+    {
     
-   double ave2 = m2.getAverage ();
+    cout << "Please enter the number of row and the seat" << endl;
+    cin >> number >> letter;
     
     
-    cout << m2.movieName << " " << m2.mpaaRate << " " << ave2 << endl;
+    switch (letter) {
+            case 'A':
+                       column=0;
+                       break;
+            case 'B':
+                       column=1;
+                       break;        
+            case 'C':
+                       column=2;
+                       break; 
+            case 'D':
+                       column=3;
+                       break;
+            default :
+            cout << "Please select within the range";
+    }
+    switch (number) {
+            case 1:
+                       row=0;
+                       break;
+            case 2:
+                       row=1;
+                       break;        
+            case 3:
+                       row=2;
+                       break; 
+            case 4:
+                       row=3;
+                       break;
+            default :
+            cout << "Please select within the range";
+    }
+   
+    
+    if (!(seat[row][column]=='X'))
+    {
+    seat[row][column]='X';
+    for ( i=0;i<7;i++)
+    {
+         cout << i+1 << " ";
+        for ( j=0;j < 4; j++)
+        {
+    cout << seat[i][j] << " ";
+        }
+       
+        
+        cout << endl;
+    }
+    cout<<"press 1 to exit or 0 to continue";
+    cin >> exit;
+    }
+    else if (seat[row][column]=='X')
+    {
+        cout << "This seat is already taken" << endl;
+        
+        
+        
+    }
+    
+    }
+    while((!(exit==1)) || (seat ==fullSeat));
+       
+        
+   
     
     
-    m3.movieName = "Toy Story 3";
-    m3.mpaaRate = "G";
-    m3.addRating (1);
-    m3.addRating (2);
-    m3.addRating (2);
-    m3.addRating (3);
-    m3.addRating (3);
-    m3.addRating (3);
-    m3.addRating (3);
-    m3.addRating (4);
-    m3.addRating (4);
-    m3.addRating (4);
-    m3.addRating (4);
-    m3.addRating (5);
-    m3.addRating (5);
-    m3.addRating (5);
-    m3.addRating (5);
     
-    
-   double ave3 = m3.getAverage ();
-    
-    
-    cout << m3.movieName << " " << m3.mpaaRate << " " << ave3 << endl;
 
     return 0;
 }
-
-void Movie::addRating (int rate )
-{
-    
-    if ((rate>=1) & (rate <=5))
-    {
-        
-        if (rate==1)
-            terrible ++ ;
-        else if (rate==2)
-            bad ++ ;
-        else if (rate==3)
-            okm ++ ;
-        else if (rate==4)
-            good ++ ;
-        else if (rate==5)
-            great ++ ;
-    }
-    else exit(1);
-}
-    
-    
- double Movie::getAverage ()
- {
-     double ave;
-     ave= static_cast<double>((terrible) + (2* bad) + (3* okm) + (4* good) + (5* great))/(terrible+bad+okm+good+great);
-              
-              return ave;  
- }
- 
- Movie::Movie (string name , string mpaa)
- {
-   movieName = name;
-   mpaaRate = mpaa;
- }
- 
- 
- 
-Movie:: Movie ()
- {
-     
- }
-    
-
 
